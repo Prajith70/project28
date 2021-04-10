0893c399933ea40d469de6d6dc1e3129e29b8e83
Prajith70 <73224732+Prajith70@users.noreply.github.com>
2021-04-10 16:25:00 +0300

    Update mango.js

--- mango.js
+++ mango.js
@@ -14,7 +14,7 @@ class Mango{
       push();
       translate(this.body.position.x, this.body.position.y);
       imageMode(CENTER);
-      image(this.image, 0, 0, this.diametre);
+      image(this.image, 0, 0,50,50);
       pop();
     }
 }
